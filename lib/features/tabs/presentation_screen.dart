19a61097c6d566d0d460a8a3104eb0b45650a29b
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-05-04 22:09:11 +0300

    Created BottomNavBar Design

--- lib/features/tabs/presentation_screen.dart
+++ lib/features/tabs/presentation_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/config/theme/app_colors.dart';
 
 class PresentationScreen extends StatelessWidget {
   const PresentationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: _bottomNavigationBarWidget(context),
+    );
+  }
+
+  Widget _bottomNavigationBarWidget(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: AppColors.bottomNavBar,
+      ),
+      child: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search_rounded), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.local_movies), label: "Browse"),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: "WatchList"),
+        ],
+      ),
+    );
   }
 }
