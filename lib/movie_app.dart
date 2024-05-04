a8387094fe94c2d179016bbd573e1778fe353f45
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-05-04 20:16:00 +0300

    Created System Design

---
+++ lib/movie_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Movie App',
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:movie_app/main.dart';
+import 'package:movie_app/movie_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MovieApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
