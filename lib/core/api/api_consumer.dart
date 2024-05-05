d32f6de4ba82cb77c0316652de77f3af6538d2ca
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-05-05 09:36:36 +0300

    Create api consumer interface

---
+++ lib/core/api/api_consumer.dart
@@ -0,0 +1,25 @@
+abstract class ApiConsumer {
+  Future<dynamic> get(String path,
+      {Map<String, dynamic>? queryParameters, Map<String, String>? headers});
+
+  Future<dynamic> post(
+    String path, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+  });
+
+  Future<dynamic> put(
+    String path, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+  });
+
+  Future<dynamic> delete(
+    String path, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+  });
+}
