65b51f872b82d086e6508bccd0fd25136e9ed7f1
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-05-09 14:17:03 +0300

    Created remote data source files for movie details and similar movies, and send movie id as a parameter to SimilarMoviesRepository

--- lib/features/movie_details/data/data_sources/remote/movie_deatils/movie_details_data_source.dart
+++ lib/features/movie_details/data/data_sources/remote/movie_deatils/movie_details_data_source.dart
@@ -1,5 +1,5 @@
 import 'package:movie_app/features/movie_details/domain/models/movie_details/movie_details.dart';
 
-abstract class MovieDetailsRepository {
+abstract class MovieDetailsOnlineDataSource {
   Future<MovieDetails>? getMovieDetails(String? movieId);
 }
