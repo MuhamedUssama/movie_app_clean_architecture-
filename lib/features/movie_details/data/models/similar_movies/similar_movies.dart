e4f1b57604cfdd5905375d01f2bc206229099ae5
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-05-09 14:00:56 +0300

    Created SimilarMovies model in data layer

---
+++ lib/features/movie_details/data/models/similar_movies/similar_movies.dart
@@ -0,0 +1,159 @@
+class SimilarMovies {
+  int? page;
+  List<Results>? results;
+  int? totalPages;
+  int? totalResults;
+  bool? success;
+  int? statusCode;
+  String? statusMessage;
+
+  SimilarMovies({
+    this.page,
+    this.results,
+    this.totalPages,
+    this.totalResults,
+    this.statusCode,
+    this.success,
+    this.statusMessage,
+  });
+
+  SimilarMovies.fromJson(dynamic json) {
+    page = json['page'];
+    if (json['results'] != null) {
+      results = [];
+      json['results'].forEach((v) {
+        results?.add(Results.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+    success = json['success'];
+    statusCode = json['status_code'];
+    statusMessage = json['status_message'];
+  }
+
+  SimilarMovies copyWith({
+    int? page,
+    List<Results>? results,
+    int? totalPages,
+    int? totalResults,
+  }) =>
+      SimilarMovies(
+        page: page ?? this.page,
+        results: results ?? this.results,
+        totalPages: totalPages ?? this.totalPages,
+        totalResults: totalResults ?? this.totalResults,
+      );
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['page'] = page;
+    if (results != null) {
+      map['results'] = results?.map((v) => v.toJson()).toList();
+    }
+    map['total_pages'] = totalPages;
+    map['total_results'] = totalResults;
+    return map;
+  }
+}
+
+class Results {
+  bool? adult;
+  String? backdropPath;
+  List<int>? genreIds;
+  int? id;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? releaseDate;
+  String? title;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+
+  Results({
+    this.adult,
+    this.backdropPath,
+    this.genreIds,
+    this.id,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+
+  Results.fromJson(dynamic json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds = json['genre_ids'] != null ? json['genre_ids'].cast<int>() : [];
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
+  }
+
+  Results copyWith({
+    bool? adult,
+    String? backdropPath,
+    List<int>? genreIds,
+    int? id,
+    String? originalLanguage,
+    String? originalTitle,
+    String? overview,
+    double? popularity,
+    String? posterPath,
+    String? releaseDate,
+    String? title,
+    bool? video,
+    double? voteAverage,
+    int? voteCount,
+  }) =>
+      Results(
+        adult: adult ?? this.adult,
+        backdropPath: backdropPath ?? this.backdropPath,
+        genreIds: genreIds ?? this.genreIds,
+        id: id ?? this.id,
+        originalLanguage: originalLanguage ?? this.originalLanguage,
+        originalTitle: originalTitle ?? this.originalTitle,
+        overview: overview ?? this.overview,
+        popularity: popularity ?? this.popularity,
+        posterPath: posterPath ?? this.posterPath,
+        releaseDate: releaseDate ?? this.releaseDate,
+        title: title ?? this.title,
+        video: video ?? this.video,
+        voteAverage: voteAverage ?? this.voteAverage,
+        voteCount: voteCount ?? this.voteCount,
+      );
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['adult'] = adult;
+    map['backdrop_path'] = backdropPath;
+    map['genre_ids'] = genreIds;
+    map['id'] = id;
+    map['original_language'] = originalLanguage;
+    map['original_title'] = originalTitle;
+    map['overview'] = overview;
+    map['popularity'] = popularity;
+    map['poster_path'] = posterPath;
+    map['release_date'] = releaseDate;
+    map['title'] = title;
+    map['video'] = video;
+    map['vote_average'] = voteAverage;
+    map['vote_count'] = voteCount;
+    return map;
+  }
+}
