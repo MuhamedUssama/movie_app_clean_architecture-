5eaa4b2d6be2a04cf11fd02577ee757b15a20b61
Mohamed Osama <mohamed.osama200538@gmail.com>
2024-05-10 15:21:22 +0300

    Used screenutil package to make movie details screen responsive

--- lib/features/movie_details/ui/movie_details_screen.dart
+++ lib/features/movie_details/ui/movie_details_screen.dart
@@ -1,9 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app/config/theme/app_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movie_app/core/di/di.dart';
 import 'package:movie_app/core/widgets/loading_widget.dart';
+import 'package:movie_app/features/movie_details/domain/models/movie_details/movie_details.dart';
 
+import '../../../core/utils/app_images.dart';
+import '../../../core/utils/app_strings.dart';
 import '../../../core/widgets/error_widget.dart';
 import 'cubits/movie_details/movie_details_states.dart';
 import 'cubits/movie_details/movie_details_view_model.dart';
@@ -24,22 +28,7 @@ class MovieDetailsScreen extends StatelessWidget {
         if (state is MovieDetailsErrorState) {
           return CustomErrorWidget(message: state.errorMessage);
         } else if (state is MovieDetailsSuccessState) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(
-                state.movieDetails!.title ?? "",
-              ),
-            ),
-            body: Center(
-              child: Text(
-                'ID: $movieId',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                ),
-              ),
-            ),
-          );
+          return buildMovieDetailsScreen(state.movieDetails!, context);
         } else {
           return SizedBox(
             height: MediaQuery.of(context).size.height,
@@ -49,6 +38,30 @@ class MovieDetailsScreen extends StatelessWidget {
           );
         }
       },
+    );
+  }
+
+  Widget buildMovieDetailsScreen(MovieDetails movie, BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          movie.title ?? "",
+        ),
+      ),
+      body: Column(
+        children: [
+          CachedNetworkImage(
+            height: 218.h,
+            width: double.infinity,
+            imageUrl: AppStrings.networkImageBaseUrl + movie.backdropPath!,
+            errorWidget: (_, __, ___) => Image.asset(
+              AppImages.imageTest,
+              height: MediaQuery.of(context).size.height * .24,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/movie_app.dart
+++ lib/movie_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movie_app/config/routes/app_router.dart';
 import 'package:movie_app/config/routes/routes_name.dart';
 
@@ -9,12 +10,18 @@ class MovieApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Movie App',
-      theme: AppTheme.theme,
-      debugShowCheckedModeBanner: false,
-      initialRoute: RoutesName.splashScreen,
-      onGenerateRoute: (settings) => AppRouters.onGenerate(settings),
+    return ScreenUtilInit(
+      // designSize: const Size(412, 892),
+      designSize: const Size(360, 820),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        title: 'Movie App',
+        theme: AppTheme.theme,
+        debugShowCheckedModeBanner: false,
+        initialRoute: RoutesName.splashScreen,
+        onGenerateRoute: (settings) => AppRouters.onGenerate(settings),
+      ),
     );
   }
 }
